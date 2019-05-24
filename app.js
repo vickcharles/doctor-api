674547c7cbc9699595807111c5c97118fd755087
vikler charles <charlesvikler@gmail.com>
2019-05-24 15:22:40 -0500

    mas ediciones

--- app.js
+++ app.js
@@ -31,4 +31,6 @@ app.use((err, req, res, next) => {
 });
 
 // start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+
+var port = process.env.PORT || 3000;
+app.listen(port , () => console.log(`Server started at port : ${port}`));
