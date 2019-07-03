4b2de7eb38e13c55aa9956cd55ef90d09a23aaba
Juan Rozo <rozoacg@gmail.com>
2019-07-02 20:25:33 -0500

    mas ediciones

--- routes/index.router.js
+++ routes/index.router.js
@@ -8,13 +8,23 @@ router.post('/register', ctrlUser.register);
 router.get('/users', ctrlUser.getAllUsers);
 
 router.get('/', (req, res) => {
-    res.send("hola mundo");
+  res.send("hola mundo");
+});
+
+
+router.get('/servidor/sumar', (req, res) => {
+  let numero1 = req.body.numero1;
+  let numero2 = req.body.numero1;
+
+  res.status(200).send({
+    resultado: numero1 + numero2
+ })
 });
 
 router.get('/users/admin', ctrlUser.getAllAdminUsers);
 router.post('/register/request', ctrlUser.registerAndPostRequest);
 router.post('/authenticate', ctrlUser.authenticate);
-router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+router.get('/userProfile', jwtHelper.verifyJwtToken, ctrlUser.userProfile);
 router.post('/user/update', jwtHelper.verifyJwtToken, ctrlUser.getUserAndUpdate);
 
 module.exports = router;
